791cd69ffd289ea724478114e0061e75ab57655a
Nick Gudenkauf <gudenkauf.nick@gmail.com>
2025-11-21 21:52:50 -0600

    Create UseRobotLocationOpMode.java
    Practice using the variables that I made in another class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UseRobotLocationOpMode.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+@Disabled
+public class UseRobotLocationOpMode extends OpMode {
+    RobotLocationPractice robotLocation = new RobotLocationPractice(0);
+
+
+    @Override
+    public void init() {
+        robotLocation.setAngle(0);
+        robotLocation.setX(0);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a){
+            robotLocation.turnRobot(0.1);
+        } else if (gamepad1.b) {
+            robotLocation.turnRobot(-0.1);
+        }
+
+
+
+        if (gamepad1.dpad_left){
+            robotLocation.changeX(0.1);
+        } else if (gamepad1.dpad_right) {
+            robotLocation.changeX(-0.1);
+        }
+        if (gamepad1.dpad_down){
+            robotLocation.changeY(0.1);
+        } else if (gamepad1.dpad_up) {
+            robotLocation.changeY(-0.1);
+        }
+        telemetry.addData("Heading", robotLocation.getHeading());
+        telemetry.addData("Angle", robotLocation.getAngle());
+        telemetry.addData("X value", robotLocation.getX());
+        telemetry.addData("Y value", robotLocation.getY());
+    }
+}
