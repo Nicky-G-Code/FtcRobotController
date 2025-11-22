b02a9a28dc1f46817f32387d2ad7900175412a6b
Nick Gudenkauf <gudenkauf.nick@gmail.com>
2025-11-21 21:50:31 -0600

    Update IfPractice.java
    Temporarily disabled.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-//@Disabled
+@Disabled
 public class IfPractice extends OpMode {
 
     @Override
@@ -15,7 +15,7 @@ public class IfPractice extends OpMode {
 
     @Override
     public void loop() {
-        boolean aButton = gamepad1.a; // press TRUE, depress FALSE
+        /*boolean aButton = gamepad1.a; // press TRUE, depress FALSE
         //true or false
         if (aButton){
             telemetry.addData("A Button", "Pressed");
@@ -23,6 +23,17 @@ public class IfPractice extends OpMode {
             else{
                 telemetry.addData("A Button", "Not Pressed");
             }
-        telemetry.addData("A Button State", aButton);
+        telemetry.addData("A Button State", aButton);*/
+
+        double leftY = gamepad1.right_stick_y;
+
+            if(leftY < 0){
+                telemetry.addData("Left Stick", "is Negative");
+            } else if (leftY > 0) {
+                telemetry.addData("Left Stick", "Is Positive");
+            }else {
+                telemetry.addData("Left Stick", "Is Zero");
+            }
+            telemetry.addData("Left Stick Value", leftY);
     }
 }
