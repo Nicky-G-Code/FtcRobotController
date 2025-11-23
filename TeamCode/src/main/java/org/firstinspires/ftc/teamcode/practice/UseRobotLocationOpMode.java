3826a3f3419a82611a4caad5e25ffe0fd1f74c14
Nick Gudenkauf <gudenkauf.nick@gmail.com>
2025-11-22 22:33:41 -0600

    Update UseRobotLocationOpMode.java
    Updated import statement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/UseRobotLocationOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/UseRobotLocationOpMode.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Mechanisms.RobotLocationPractice;
+
 @TeleOp
 @Disabled
 public class UseRobotLocationOpMode extends OpMode {
